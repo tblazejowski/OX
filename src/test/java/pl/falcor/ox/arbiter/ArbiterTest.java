4b9f91f871b58c61bcf2a527ba3d99ab502fc597
Tomasz Blazejowski <tomasz_blazejowski@epam.com>
2018-11-30 14:05:39 +0100

    bundle of tests added to arbiter for different winning scenarios

--- src/test/java/pl/falcor/ox/arbiter/ArbiterTest.java
+++ src/test/java/pl/falcor/ox/arbiter/ArbiterTest.java
@@ -11,30 +11,132 @@ import pl.falcor.ox.board.Sign;
 
 public class ArbiterTest {
 
-    private Board board;
-    private Arbiter arbiter;
+    private Board[] boards;
+    private Arbiter[] arbiters;
 
     @BeforeTest
     public void initialize() {
-        board = new Board(new BoardDimension(4));
-        arbiter = new Arbiter(board);
+        boards = new Board[7];
+        arbiters = new Arbiter[7];
+        for (int i = 0; i < boards.length; i++) {
+            boards[i] = new Board(new BoardDimension(5));
+            arbiters[i] = new Arbiter(boards[i]);
+        }
     }
+
 
     @DataProvider(name = "testWinningSequenceOfXsInRow")
     public static Object[][] winningSequenceOfXsInRow() {
         return new Object[][]{
-                {new Field(5), false},
                 {new Field(6), false},
                 {new Field(7), false},
-                {new Field(8), true},
+                {new Field(8), false},
+                {new Field(9), false},
+                {new Field(10), true},
         };
     }
 
     @Test(dataProvider = "testWinningSequenceOfXsInRow")
     public void shouldConfirmMatchWonWhenAllXSignsInTheSameRow(Field field, boolean result) {
 
-        board.addSign(field, Sign.X);
+        boards[0].addSign(field, Sign.X);
 
-        Assert.assertEquals(arbiter.isWinningSign(field, Sign.X), result);
+        Assert.assertEquals(arbiters[0].isWinningSign(field, Sign.X), result);
+    }
+
+    @DataProvider(name = "testWinningSequenceOfXsInColumn")
+    public static Object[][] winningSequenceOfXsInColumn() {
+        return new Object[][]{
+                {new Field(4), false},
+                {new Field(9), false},
+                {new Field(14), false},
+                {new Field(19), false},
+                {new Field(24), true},
+        };
+    }
+
+    @Test(dataProvider = "testWinningSequenceOfXsInColumn")
+    public void shouldConfirmMatchWonWhenAllXSignsInTheSameColumn(Field field, boolean result) {
+
+        boards[1].addSign(field, Sign.X);
+
+        Assert.assertEquals(arbiters[1].isWinningSign(field, Sign.X), result);
+    }
+
+    @DataProvider(name = "testWinningSequenceOfXsInMajorDiagonal")
+    public static Object[][] winningSequenceOfXsInMajorDiagonal() {
+        return new Object[][]{
+                {new Field(1), false},
+                {new Field(7), false},
+                {new Field(13), false},
+                {new Field(19), false},
+                {new Field(25), true},
+        };
+    }
+
+    @Test(dataProvider = "testWinningSequenceOfXsInMajorDiagonal")
+    public void shouldConfirmMatchWonWhenAllXSignsInTheMajorDiagonal(Field field, boolean result) {
+
+        boards[2].addSign(field, Sign.X);
+
+        Assert.assertEquals(arbiters[2].isWinningSign(field, Sign.X), result);
+    }
+
+    @DataProvider(name = "testWinningSequenceOfXsInMinorDiagonal")
+    public static Object[][] winningSequenceOfXsInMinorDiagonal() {
+        return new Object[][]{
+                {new Field(5), false},
+                {new Field(9), false},
+                {new Field(13), false},
+                {new Field(17), false},
+                {new Field(21), true},
+        };
+    }
+
+    @Test(dataProvider = "testWinningSequenceOfXsInMinorDiagonal")
+    public void shouldConfirmMatchWonWhenAllXSignsInTheMinorDiagonal(Field field, boolean result) {
+
+        boards[3].addSign(field, Sign.X);
+
+        Assert.assertEquals(arbiters[3].isWinningSign(field, Sign.X), result);
+    }
+
+    @DataProvider(name = "testBoardIsFulfilledAndNoWinningConditionIsMet")
+    public static Object[][] boardIsFulfilledAndNoWinningConditionIsMet() {
+        return new Object[][]{
+                {new Field(1), Sign.O},
+                {new Field(2), Sign.X},
+                {new Field(3), Sign.O},
+                {new Field(4), Sign.X},
+                {new Field(5), Sign.O},
+                {new Field(6), Sign.O},
+                {new Field(7), Sign.X},
+                {new Field(8), Sign.O},
+                {new Field(9), Sign.X},
+                {new Field(10), Sign.O},
+                {new Field(11), Sign.O},
+                {new Field(12), Sign.X},
+                {new Field(13), Sign.O},
+                {new Field(14), Sign.X},
+                {new Field(15), Sign.O},
+                {new Field(16), Sign.O},
+                {new Field(17), Sign.X},
+                {new Field(18), Sign.O},
+                {new Field(19), Sign.X},
+                {new Field(20), Sign.O},
+                {new Field(21), Sign.X},
+                {new Field(22), Sign.O},
+                {new Field(23), Sign.X},
+                {new Field(24), Sign.O},
+                {new Field(25), Sign.X},
+        };
+    }
+
+    @Test(dataProvider = "testBoardIsFulfilledAndNoWinningConditionIsMet")
+    public void shouldConfirmBoardIsFulfilledAndNoWinningConditionIsMet(Field field, Sign sign) {
+
+        boards[4].addSign(field, sign);
+
+        Assert.assertEquals(arbiters[4].isWinningSign(field, sign), false);
     }
 }
